1f9e2ea1a16ca4a6935eaa46aabeb0e2534470b0
javabipe <67403018+javabipe@users.noreply.github.com>
2022-06-25 09:25:17 -0300

    Add files via upload

---
+++ public/settings.js
@@ -0,0 +1,7 @@
+window.rekognitionSettings = {
+  apiGateway: "https://bugxg30oz0.execute-api.us-east-1.amazonaws.com/Prod/",
+  cognitoIdentityPool: "us-east-1:7d3a4772-90ca-4597-a064-6ad25526f793",
+  cognitoUserPoolId: "us-east-1_9MRDTwniw",
+  cognitoUserPoolClientId: "5qiqchrrna9usmc1ji02dbncs1",
+  region: "us-east-1",
+};
